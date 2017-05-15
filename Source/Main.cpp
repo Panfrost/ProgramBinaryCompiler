fa426f6e9a0ca3b65b9010c4c2af77ae1059400b
BiOpenly <BiOpenly@gmail.com>
2017-05-14 19:30:49 -0700

    Fixes issue #1. Trims the strings.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <array>
 #include <fstream>
 #include <stdio.h>
@@ -19,9 +20,30 @@ enum ShaderType
 	TYPE_MERGED,
 };
 
-GLuint compileShader(GLuint type, const std::string& source)
+// trim from start
+static inline std::string &ltrim(std::string &s) {
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+	std::not1(std::ptr_fun<int, int>(std::isspace))));
+	return s;
+}
+
+// trim from end
+static inline std::string &rtrim(std::string &s) {
+	s.erase(std::find_if(s.rbegin(), s.rend(),
+	std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+	return s;
+}
+
+// trim from both ends
+static inline std::string &trim(std::string &s) {
+	return ltrim(rtrim(s));
+}
+
+GLuint compileShader(GLuint type, std::string& source)
 {
 	GLuint result = glCreateShader(type);
+
+	source = trim(source);
 
 	const char* src[] = { source.c_str() };
 
